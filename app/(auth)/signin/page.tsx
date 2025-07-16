c1e2ac038a9fb24573a065ecf7d58792b02c8e03
Milos Stojsavljevic <milosstojsavljevic93@gmail.com>
2025-07-16 13:02:59 +0200

    Add shadcn component library

---
+++ app/(auth)/signin/page.tsx
@@ -0,0 +1,10 @@
+import { Button } from "@/components/ui/button";
+
+export default function SignInPage() {
+  return (
+    <div>
+      SignInPage
+      <Button>Click me</Button>
+    </div>
+  );
+}
