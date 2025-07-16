807a0106123535bdafa64b49388a89a7f78dbc6b
Milos Stojsavljevic <milosstojsavljevic93@gmail.com>
2025-07-16 14:04:51 +0200

    Add prisma schema and first migration

--- lib/env.ts
+++ lib/env.ts
@@ -5,6 +5,7 @@ export const env = createEnv({
   server: {
     BETTER_AUTH_URL: z.string().min(1),
     BETTER_AUTH_SECRET: z.string().min(1),
+    DATABASE_URL: z.string().min(1),
   },
   client: {},
   experimental__runtimeEnv: {},
