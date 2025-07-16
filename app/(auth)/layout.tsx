8e2277aafea70c3efc3ae4b1ebe6f6d2e2553839
Milos Stojsavljevic <milosstojsavljevic93@gmail.com>
2025-07-16 13:15:01 +0200

    Add simple login screen

--- app/(auth)/layout.tsx
+++ app/(auth)/layout.tsx
@@ -2,9 +2,8 @@ import { ReactNode } from "react";
 
 export default function AuthLayout({ children }: { children: ReactNode }) {
   return (
-    <div>
-      <h1>Auth Navigatino</h1>
-      {children}
+    <div className="bg-background flex min-h-svh flex-col items-center justify-center gap-6 p-6 md:p-10">
+      <div className="w-full max-w-sm">{children}</div>
     </div>
   );
 }
