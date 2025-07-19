ca1b278175150b1c869770f5014628872215a95b
Milos Stojsavljevic <milosstojsavljevic93@gmail.com>
2025-07-19 13:29:29 +0200

    Fix dashboard layout and components

--- app/(doctor)/dashboard/_components/main-content.tsx
+++ app/(doctor)/dashboard/_components/main-content.tsx
@@ -1,13 +1,14 @@
 import { StatsCard, StatsGrid } from "@/components/stats-grid";
 import React from "react";
 import { NavigationItem } from "./navigation-item";
+import { ScrollArea } from "@/components/ui/scroll-area";
 
 export default function DoctorDashboardMainContent() {
   const stats = [
     {
       id: 1,
       title: "Erhalt der Studienanfrage",
-      checked: false,
+      checked: true,
       date: null,
     },
     {
@@ -138,28 +139,37 @@ export default function DoctorDashboardMainContent() {
   ];
 
   return (
-    <div className="grid grid-cols-1 min-[1200px]:grid-cols-3 gap-2">
+    <div className="grid grid-cols-1 min-[1200px]:grid-cols-3 gap-6">
       <div className="flex flex-col rounded-xl bg-gradient-to-br from-sidebar/60 to-sidebar">
         <div className="relative p-4 lg:p-5 group">
-          <h1>Navigation</h1>
+          <h1 className="text-xl font-semibold">Navigation</h1>
         </div>
 
-        {stats.map((stat) => (
-          <div
-            key={stat.id}
-            className="bg-gradient-to-r hover:bg-transparent hover:from-sidebar-accent hover:to-sidebar-accent/40 data-[active=true]:from-primary/20 data-[active=true]:to-primary/5 rounded-xl"
-          >
-            <NavigationItem key={stat.id} {...stat} />
-          </div>
-        ))}
+        <ScrollArea
+          className="w-full rounded-md"
+          style={{ height: "calc(100vh - 400px)" }}
+        >
+          {stats.map((stat) => (
+            <div
+              key={stat.id}
+              className="bg-gradient-to-r hover:bg-transparent hover:from-sidebar-accent hover:to-sidebar-accent/40 data-[active=true]:from-primary/20 data-[active=true]:to-primary/5 rounded-xl"
+            >
+              <NavigationItem key={stat.id} {...stat} />
+            </div>
+          ))}
+        </ScrollArea>
+      </div>
+
+      <div className="flex flex-col rounded-xl bg-gradient-to-br from-sidebar/60 to-sidebar">
+        <h1>LEPIII</h1>
       </div>
 
       <div className="flex flex-col rounded-xl bg-gradient-to-br from-sidebar/60 to-sidebar">
         <h1>qwbeqweb</h1>
       </div>
 
-      <div className="flex flex-col rounded-xl bg-gradient-to-br from-sidebar/60 to-sidebar">
-        <h1>Stats</h1>
+      <div className="flex flex-col rounded-xl bg-gradient-to-br from-sidebar/60 to-sidebar p-4 lg:p-5">
+        <h1>Samo stats</h1>
       </div>
     </div>
   );
--- components/stats-grid.tsx
+++ components/stats-grid.tsx
@@ -16,14 +16,14 @@ export function StatsCard({ title, value, change, icon }: StatsCardProps) {
   const trendColor = isPositive ? "text-emerald-500" : "text-red-500";
 
   return (
-    <div className="relative p-4 lg:p-5 group before:absolute before:inset-x-8 before:bottom-0 before:h-px before:bg-gradient-to-r before:from-input/30 before:via-input before:to-input/30 last:before:hidden">
+    <div className="relative p-4 lg:p-5 group before:absolute before:inset-y-8 before:right-0 before:w-px before:bg-gradient-to-b before:from-input/30 before:via-input before:to-input/30">
       <div className="relative flex items-center gap-4">
         <RiArrowRightUpLine
           className="absolute right-0 top-0 opacity-0 group-has-[a:hover]:opacity-100 transition-opacity text-emerald-500"
           size={20}
           aria-hidden="true"
         />
-        <div className="max-[480px]:hidden size-10 shrink-0 rounded-full bg-emerald-600/25 border border-emerald-600/50 flex items-center justify-center text-emerald-500">
+        <div className="max-[480px]:hidden size-10 shrink-0 rounded-full bg-emerald-600/25 flex items-center justify-center text-emerald-500">
           {icon}
         </div>
         <div>
@@ -52,7 +52,7 @@ interface StatsGridProps {
 
 export function StatsGrid({ stats }: StatsGridProps) {
   return (
-    <div className="grid grid-cols-2 min-[1200px]:grid-cols-4 border border-border rounded-xl bg-gradient-to-br from-sidebar/60 to-sidebar">
+    <div className="grid grid-cols-2 min-[1200px]:grid-cols-4 rounded-xl bg-gradient-to-br from-sidebar/60 to-sidebar">
       {stats.map((stat) => (
         <StatsCard key={stat.title} {...stat} />
       ))}
