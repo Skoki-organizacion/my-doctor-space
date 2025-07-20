2cd09ca033f4755a6aad037baa1ac976f6462a88
Milos Stojsavljevic <milosstojsavljevic93@gmail.com>
2025-07-20 22:45:15 +0200

    Add caching for fetching studies

--- app/(admin)/admin/doctors/page.tsx
+++ app/(admin)/admin/doctors/page.tsx
@@ -1,11 +1,15 @@
 import DashboardHeader from "@/components/dashboard-header";
 import { SidebarInset } from "@/components/ui/sidebar";
+import AdminStatsGrid from "../dashboard/_components/admin-stats-grid";
+import AdminDashboardTitle from "../dashboard/_components/admin-title";
 
 export default function DoctorsPage() {
   return (
     <SidebarInset className="overflow-hidden px-4 md:px-6 lg:px-8">
       <div className="flex flex-1 flex-col gap-4 lg:gap-6">
         <DashboardHeader />
+        <AdminDashboardTitle />
+        <AdminStatsGrid />
       </div>
     </SidebarInset>
   );
--- app/data/admin/get-studies.ts
+++ app/data/admin/get-studies.ts
@@ -3,29 +3,33 @@
 import { prisma } from "@/lib/db";
 import { requireAdmin } from "./require-admin";
 import { notFound } from "next/navigation";
+import { cache } from "react";
 
 export default async function getAllStudies() {
   await requireAdmin();
 
-  const data = await prisma.doctorInfo.findMany({
-    select: {
-      id: true,
-      clinic: true,
-      department: true,
-      study: true,
-      user: {
+  const data = cache(
+    async () =>
+      await prisma.doctorInfo.findMany({
         select: {
           id: true,
+          clinic: true,
+          department: true,
+          study: true,
+          user: {
+            select: {
+              id: true,
+            },
+          },
         },
-      },
-    },
-  });
+      })
+  );
 
   if (!data) {
     return notFound();
   }
 
-  return data;
+  return await data();
 }
 
-export type GetAllStudiesType = Awaited<ReturnType<typeof getAllStudies>>;
+export type GetAllStudiesType = Awaited<ReturnType<typeof getAllStudies>>[0];
