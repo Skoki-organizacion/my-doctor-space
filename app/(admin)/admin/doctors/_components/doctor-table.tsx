d005052ac7bcb73a78a1977b2fb3c384dbae4981
Milos Stojsavljevic <milosstojsavljevic93@gmail.com>
2025-07-23 14:05:30 +0200

    Fix styling on the delete buttons

--- app/(admin)/admin/doctors/_components/doctor-table.tsx
+++ app/(admin)/admin/doctors/_components/doctor-table.tsx
@@ -340,12 +340,10 @@ export default function DoctorsTable({ doctors }: iAppProps) {
                   </AlertDialogHeader>
                 </div>
                 <AlertDialogFooter>
-                  <AlertDialogCancel className="bg-destructive hover:bg-destructive-foreground cursor-pointer">
-                    Cancel
-                  </AlertDialogCancel>
+                  <AlertDialogCancel>Cancel</AlertDialogCancel>
                   <AlertDialogAction
                     onClick={handleDeleteRows}
-                    className="cursor-pointer"
+                    className="bg-destructive text-white shadow-xs hover:bg-destructive/90 focus-visible:ring-destructive/20 dark:focus-visible:ring-destructive/40"
                   >
                     Delete
                   </AlertDialogAction>
