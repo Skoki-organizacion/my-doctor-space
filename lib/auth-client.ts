9f63424b8fb03a1e1ace985fe2ac5932f1386d2f
Milos Stojsavljevic <milosstojsavljevic93@gmail.com>
2025-07-30 17:41:31 +0200

    Fix base url for auth client

--- lib/auth-client.ts
+++ lib/auth-client.ts
@@ -2,6 +2,6 @@ import { createAuthClient } from "better-auth/react";
 import { adminClient } from "better-auth/client/plugins";
 
 export const authClient = createAuthClient({
-  baseURL: "http://localhost:3000",
+  baseURL: "https://my-doctor-space.vercel.app",
   plugins: [adminClient()],
 });
