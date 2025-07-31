b3790e8c1146fd736a3888421c4250196f80a969
Milos Stojsavljevic <milosstojsavljevic93@gmail.com>
2025-07-31 18:19:57 +0200

    Add hero page

--- components/landing-page/hero.tsx
+++ components/landing-page/hero.tsx
@@ -1,5 +1,6 @@
 import { Button } from "@/components/ui/button";
 import { ArrowRight, Play } from "lucide-react";
+import Link from "next/link";
 
 export function Hero() {
   return (
@@ -7,24 +8,21 @@ export function Hero() {
       <div className="container mx-auto text-center">
         <div className="max-w-4xl mx-auto">
           <h1 className="text-4xl sm:text-5xl lg:text-6xl font-bold text-foreground mb-6 leading-tight">
-            Automate Smarter. Grow
-            <br />
-            Faster. <span className="text-primary">Why AI</span>
+            Where Quality Meets
+            <span className="text-primary"> Care</span>.
           </h1>
 
           <p className="text-lg sm:text-xl text-muted-foreground mb-8 max-w-2xl mx-auto">
-            Transform your business with intelligent automation solutions that
-            scale with your growth
+            Confirmation of strategy and concept from the best-known leaders in
+            the field of clinical research in verified confirmed circumstances.
           </p>
 
           <div className="flex flex-col sm:flex-row items-center justify-center gap-4 mb-12">
-            <Button size="lg" className="w-full sm:w-auto">
-              Get Started <ArrowRight className="ml-2 h-4 w-4" />
-            </Button>
-            <Button variant="outline" size="lg" className="w-full sm:w-auto">
-              <Play className="mr-2 h-4 w-4" />
-              Watch Demo
-            </Button>
+            <Link href={"/sign-in"}>
+              <Button size="lg" className="w-full sm:w-auto">
+                Get Started <ArrowRight className="ml-2 h-4 w-4" />
+              </Button>
+            </Link>
           </div>
 
           <div className="relative max-w-4xl mx-auto">
--- lib/auth-client.ts
+++ lib/auth-client.ts
@@ -2,6 +2,6 @@ import { createAuthClient } from "better-auth/react";
 import { adminClient } from "better-auth/client/plugins";
 
 export const authClient = createAuthClient({
-  baseURL: "https://my-doctor-space.vercel.app",
+  baseURL: "http://localhost:3000",
   plugins: [adminClient()],
 });
