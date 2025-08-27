859156411ad7256fdcb6726a5add64a3ed1b18e3
Milos Stojsavljevic <milosstojsavljevic93@gmail.com>
2025-08-27 20:05:08 +0200

    Add doctor dashboard to the admin studies page

--- app/(admin)/admin/studies/[id]/page.tsx
+++ app/(admin)/admin/studies/[id]/page.tsx
@@ -1,3 +1,4 @@
+import DoctorDashboardMainContent from "@/app/(doctor)/dashboard/[id]/_components/main-content";
 import { getStudy } from "@/app/data/admin/get-study";
 import DashboardHeader from "@/components/dashboard-header";
 import { SidebarInset } from "@/components/ui/sidebar";
@@ -6,14 +7,15 @@ type Params = Promise<{ ["id"]: string }>;
 
 export default async function StudyDetailsPage({ params }: { params: Params }) {
   const { id } = await params;
-  const study = getStudy(id);
+  const studyDetails = await getStudy(id);
 
   return (
     <SidebarInset className="overflow-hidden px-4 md:px-6 lg:px-8">
       <div className="flex flex-1 flex-col gap-4 lg:gap-6">
         <DashboardHeader />
-
-        {JSON.stringify(study)}
+        <div className="flex flex-1 flex-col gap-4 lg:gap-6 py-4 lg:py-6">
+          <DoctorDashboardMainContent studyDetails={studyDetails} />
+        </div>
       </div>
     </SidebarInset>
   );
