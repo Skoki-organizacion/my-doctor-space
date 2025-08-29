6b66357d238cb30d5d86284c0689a026e6a53af7
Milos Stojsavljevic <milosstojsavljevic93@gmail.com>
2025-08-29 19:52:42 +0200

    Index user and session to speed up authentication

--- lib/db.ts
+++ lib/db.ts
@@ -1,7 +1,16 @@
+import { env } from "./env";
 import { PrismaClient } from "./generated/prisma";
 
 const globalForPrisma = global as unknown as { prisma: PrismaClient };
 
-export const prisma = globalForPrisma.prisma || new PrismaClient();
+export const prisma =
+  globalForPrisma.prisma ||
+  new PrismaClient({
+    datasources: {
+      db: {
+        url: env.DATABASE_URL,
+      },
+    },
+  });
 
 if (process.env.NODE_ENV !== "production") globalForPrisma.prisma = prisma;
