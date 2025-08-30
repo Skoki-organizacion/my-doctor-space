4bf6ae3247bc3bcef3d05803a8e13ad0bf2d691c
Milos Stojsavljevic <milosstojsavljevic93@gmail.com>
2025-08-30 19:23:02 +0200

    Add proper size for hero

--- components/landing-page/hero.tsx
+++ components/landing-page/hero.tsx
@@ -30,33 +30,35 @@ export async function Hero() {
 
       <div className="absolute inset-0 bg-gradient-to-t from-background to-background/25" />
 
-      <div className="relative z-10 text-center px-4 sm:px-6 lg:px-8 max-w-4xl mx-auto">
-        <h1
-          className={`${playfairDisplay.className} text-4xl sm:text-5xl lg:text-8xl font-bold text-white mb-6 leading-tight`}
-        >
-          Where Quality Meets
-          <span className="text-primary"> Care</span>.
-        </h1>
-
-        <p className="text-lg sm:text-xl text-gray-200 mb-8 max-w-2xl mx-auto">
-          Confirmation of strategy and concept from the best-known leaders in
-          the field of clinical research in verified confirmed circumstances.
-        </p>
-
-        <div className="flex flex-col sm:flex-row items-center justify-center gap-4">
-          <Link
-            href={
-              session
-                ? session?.user.email === "milosstojsavljevic93@gmail.com"
-                  ? "/admin/dashboard"
-                  : "/dashboard"
-                : "/sign-in"
-            }
+      <div className="absolute bottom-32 left-1/2 transform -translate-x-1/2 z-10 w-full">
+        <div className="relative z-10 text-center px-4 sm:px-6 lg:px-8 max-w-4xl mx-auto">
+          <h1
+            className={`${playfairDisplay.className} text-4xl sm:text-5xl lg:text-8xl font-bold text-white mb-6 leading-tight`}
           >
-            <Button size="lg" className="w-full sm:w-auto opacity-100 z-10">
-              Get Started <ArrowRight className="ml-2 h-4 w-4" />
-            </Button>
-          </Link>
+            Where Quality Meets
+            <span className="text-primary"> Care</span>.
+          </h1>
+
+          <p className="text-lg sm:text-xl text-gray-200 mb-8 max-w-2xl mx-auto">
+            Confirmation of strategy and concept from the best-known leaders in
+            the field of clinical research in verified confirmed circumstances.
+          </p>
+
+          <div className="flex flex-col sm:flex-row items-center justify-center gap-4">
+            <Link
+              href={
+                session
+                  ? session?.user.email === "milosstojsavljevic93@gmail.com"
+                    ? "/admin/dashboard"
+                    : "/dashboard"
+                  : "/sign-in"
+              }
+            >
+              <Button size="lg" className="w-full sm:w-auto opacity-100 z-10">
+                Get Started <ArrowRight className="ml-2 h-4 w-4" />
+              </Button>
+            </Link>
+          </div>
         </div>
       </div>
     </section>
